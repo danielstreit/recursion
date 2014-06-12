ca7970e4d433faec39529ceba54d6254d2dfdd53
Daniel Streit <Daniel.Streit@gmail.com>
2014-06-12 14:33:25 -0700

    Implement getElementsByClassName

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -20,6 +20,14 @@ describe('getElementsByClassName', function(){
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
+      // We cannot use '===' because arrays are stored by reference unlike primitive
+      // data types (bool, num, string) that are stored by value. === would check if
+      // the reference of both arrays were the same. They are not. The arrays were created
+      // seperately and are completely different instances of arrays. They are held in 
+      // different spots in memory. The different references point to these different
+      // spots in memory. The underscore library provides a utility to test for "deep" 
+      // equality of arrays (or objects). This means that two arrays contain the same values
+      // even if they are different instances of those values.
       expect(equality).to.equal(true);
 
       $rootElement.remove();
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,23 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var current = arguments[1] || document.body;
+  var result = arguments[2] || [];
+
+  if (current.classList.contains(className)) {
+  	result.push(current);
+  }
+
+  // This may also have been implemented using current.children instead of 
+  // the required current.childNodes. This would have eliminated the need
+  // for the conditional checking that the node is an element node.
+  // Element.children only returns the elements (what we want).
+  // Node.childNodes returns all child nodes, including text nodes (we don't want those)
+  Array.prototype.slice.apply(current.childNodes).forEach(function(node) {
+  	if (node.nodeType === Node.ELEMENT_NODE) {
+  		getElementsByClassName(className, node, result);
+  	}
+  });
+
+  return result;
 };
